76b1e403647e732f7202d504e7c0e878d49f23bd
Prashant <prashantyallatti79968@gmail.com>
2023-06-27 23:13:52 +0530

    Setup folder structure of project and stuctured and design required components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import List from "./List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="dropdown-item">
+        <h2 className="que">Should you use dropdown?</h2>
+        <button>
+          <p>Select</p>
+          <span>
+            <i class="fa-solid fa-angle-down"></i>
+          </span>
+        </button>
+        <List />
+      </div>
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div className="drop-list">
+      <ul>
+        <li>Yes</li>
+        <li>Probably</li>
+      </ul>
+    </div>
+  );
+};
+
+export default List;
